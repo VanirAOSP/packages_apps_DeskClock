f675395469fb87d41b599b09f9deb03c1cb386e3
nuclearmistake <nuclearmistake@gmail.com>
2013-02-14 07:34:47 -0500

    Fix alarm dismissal/snooze/full-screen glowpad screen
    Change-Id: I6311bc0158b276115ecfa913637f832f7a2d4b06

--- src/com/android/deskclock/AlarmAlertFullScreen.java
+++ src/com/android/deskclock/AlarmAlertFullScreen.java
@@ -447,7 +447,6 @@ public class AlarmAlertFullScreen extends Activity implements GlowPadView.OnTrig
             getSensorManager().unregisterListener(mShakeListener);
             mShakeListener = null;
         }
-        finish();
     }
 
 
